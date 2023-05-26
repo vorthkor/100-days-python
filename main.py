79d448ee1bf3dd9571ae61e288c389152cb67b5b
vorthkor <12147415-vorthkor@users.noreply.replit.com>
2023-05-26 16:23:13 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,27 @@
+print("""
+*****************************************
+* GUESS THE NUMBER - ONE-MILLION-TO-ONE *
+*****************************************
+""")
+
+number = 3012
+attempts = 0
+
+while True:
+  guess = float(input("What is your guess?: "))
+  if guess > number:
+    print("Too High 🧐")
+    attempts += 1
+  elif guess < number and guess >= 0:
+    print("Too Low 😭")
+    attempts += 1
+    continue
+  elif guess < 0:
+    print("no negative numbers! 🎃 end of game!")
+    exit()
+  else:
+    attempts += 1
+    break
+
+print(f"You got it! With only {attempts} guesses! 😎")
+    

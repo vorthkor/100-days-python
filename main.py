8d6b9e22f10a8b8bc9480ea0495302fb20fa86f0
vorthkor <31862750+vorthkor@users.noreply.github.com>
2023-05-30 00:11:19 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,29 @@
+import random
+
+print("""
+*****************************************
+* GUESS THE NUMBER - ONE-MILLION-TO-ONE *
+*****************************************
+""")
+
+number = random.randint(1,1000000)
+attempts = 0
+
+while True:
+  guess = float(input("What is your guess?: "))
+  if guess > number:
+    print("Too High 🧐")
+    attempts += 1
+  elif guess < number and guess >= 0:
+    print("Too Low 😭")
+    attempts += 1
+    continue
+  elif guess < 0:
+    print("no negative numbers! 🎃 end of game!")
+    exit()
+  else:
+    attempts += 1
+    break
+
+print(f"You got it! With only {attempts} guesses! 😎")
+    
